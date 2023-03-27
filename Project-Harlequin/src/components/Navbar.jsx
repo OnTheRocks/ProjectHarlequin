81d51430a972423f9db3465178ee4ffb48e1413f
OnTheRocks <nathan_huber@yahoo.com>
2023-03-27 00:01:55 -0500

    Cleaned up some NavBar code.

--- Project-Harlequin/src/components/Navbar.jsx
+++ Project-Harlequin/src/components/Navbar.jsx
@@ -6,7 +6,11 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false)
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt="hoobank" className='w-[124px] [h-32px]' />
+      <img 
+        src={logo} 
+        alt="hoobank" 
+        className='w-[124px] [h-32px]' 
+      />
 
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
@@ -20,7 +24,6 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-
       <div className="sm:hidden flex flex-1 justify-end items-center">
         <img 
           src={toggle ? close : menu} 
@@ -28,7 +31,6 @@ const Navbar = () => {
           className='w-[28px] h-[28px] object-contain'
           onClick={() => setToggle((prev) => !prev)} 
         />
-
         <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
           <ul className='list-none flex flex-col justify-end items-center flex-1'>
             {navLinks.map((nav, index) => (
